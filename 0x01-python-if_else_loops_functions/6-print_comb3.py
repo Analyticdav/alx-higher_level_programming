12967bcd04238230a7138682e5d281b5f0964618
David Akpan <Davidakpan247@gmail.com>
2023-09-05 14:37:00 -0700

    update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for p in range(9):
     for q in range(p + 1, 10):
         if p < 8:
-            print(f"{p}{q}", end=", ")
+            print("{}{}".format(p,q), end=", ")
         else:
-            print(f"{p}{q}")
+            print("{}{}".format(p,q))
