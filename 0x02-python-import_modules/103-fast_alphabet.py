91eaa5d3606fb0f59869ae46abe371c01ff38a46
David Akpan <Davidakpan247@gmail.com>
2023-09-07 11:16:00 -0700

    update

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-
-print(chr(65) if (lambda f, x: f(f, x))(lambda self, n: chr(n) + self(self, n + 1) if n < 91 else '', 65) else '')
+import string
+print(string.ascii_uppercase)
