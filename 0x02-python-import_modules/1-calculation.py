5a9bcf42dd1992b1b4ef3405ecabf9d8c5cc7dce
David Akpan <Davidakpan247@gmail.com>
2023-09-07 06:08:26 -0700

    module calculations

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+
+    b = 5
+
+    """ Addition of a and b """
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+    """subtraction of a and b """
+
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+
+    """Multiplication of a and b """
+
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    
+    """Division of a and b """
+
+    print("{} / {} = {}".format(a, b, div(a, b)))
