84bec1c0853ec629ea1f11776b7fcb95027ee588
David Akpan <Davidakpan247@gmail.com>
2023-09-09 09:47:59 -0700

    floating codes

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print("{[39:67]}{[107:112]}{[0:6]}".format(str))
+ language that combines remarkable power with very clear syn)
+print(str)
