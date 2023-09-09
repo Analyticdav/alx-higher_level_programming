d9bc4716183da68537db7e238bb0ddf23f0dd1f1
David Akpan <Davidakpan247@gmail.com>
2023-09-09 08:31:42 -0700

    Weekends are for more debugging

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,9 +2,9 @@
 # 11-delete_at.py
 
 def delete_at(my_list=[], idx=0):
-
-    if idx >= 0 and idx < len(my_list):
-        
+    list_length = len(my_list) - 1
+    if idx >= 0 or idx < list_length:
+        return (my_list)
+    else:
         del my_list[idx]
-
         return (my_list)
