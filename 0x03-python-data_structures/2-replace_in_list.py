7c0dc1ef968509f00248e489028fa4ad27a70b1c
David Akpan <Davidakpan247@gmail.com>
2023-09-09 02:51:40 -0700

    update

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
-def element_at(my_list, idx):
+def element_at(my_list, element):
 
     list_length = len(my_list) - 1
 
     if (idx < 0 or idx > list_length):
 
-        return (None)
+        return (my_list)
 
     else:
 
-        return (my_list[idx])
+        (my_list[idx]) = element
+
+        return (my_list)
