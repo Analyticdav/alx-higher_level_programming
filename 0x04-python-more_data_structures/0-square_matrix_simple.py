7b0613be548dcd33094ba761c1ec5d69eccbf06b
David Akpan <Davidakpan247@gmail.com>
2023-09-13 07:28:25 -0700

    update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[q ** 2 for q in p] for p in matrix]
-    return (new_matrixi)
-
+    result_matrix = []
+    for row in matrix:
+        result_row = []
+    for value in row:
+        result_row.append(value ** 2)
+        result_matrix.append(result_row)
+    
+    return (result_matrix)
