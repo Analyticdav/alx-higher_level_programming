98aff43a8c898185931da795f86e255a7c0671dc
David Akpan <Davidakpan247@gmail.com>
2023-09-25 03:09:58 -0700

    Safe printing of values

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    x_values = 0
+    try:
+        for items in range(0, x):
+            if isinstance(my_list[items], int):
+                print("{:d}".format(my_list[items]), end='')
+                x_values = x_values + 1
+        print()
+        return (x_values)
+    except TypeError:
+        print()
