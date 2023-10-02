5a737253ef5febfe5e4649219042d3c6de14f5b3
David Akpan <Davidakpan247@gmail.com>
2023-10-02 11:28:01 -0700

    queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""" The queen's chess"""
-
 import sys
 
 def is_safe(board, row, col, n):
-	"""Check if there is a queen in the same column"""
+    """Check if there is a queen in the same column"""
     for i in range(row):
         if board[i][col] == 1:
             return False
 
-	"""Check upper left diagonal"""
+    """Check upper left diagonal"""
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
-        """Check upper right diagonal"""
+    """ Check upper right diagonal"""
     for i, j in zip(range(row, -1, -1), range(col, n)):
         if board[i][j] == 1:
             return False
