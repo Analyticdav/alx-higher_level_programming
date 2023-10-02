d3fd9938e4f368e0810f23d22b41e0c152941498
David Akpan <Davidakpan247@gmail.com>
2023-10-02 11:53:52 -0700

    update

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,59 +1,86 @@
 #!/usr/bin/python3
-import sys
+""" Queen's chess"""
 
-def is_safe(board, row, col, n):
-    """Check if there is a queen in the same column"""
-    for i in range(row):
-        if board[i][col] == 1:
-            return False
 
-    """Check upper left diagonal"""
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
+from sys import argv
 
-    """ Check upper right diagonal"""
-    for i, j in zip(range(row, -1, -1), range(col, n)):
-        if board[i][j] == 1:
-            return False
+if len(argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
 
+if not argv[1].isdigit():
+    print('N must be a number')
+    exit(1)
+
+N = int(argv[1])
+
+if N < 4:
+    print('N must be at least 4')
+    exit(1)
+
+
+def board_column_gen(board=[]):
+    """Adds a column of zero"""
+    if len(board):
+        for row in board:
+            row.append(0)
+    else:
+        for row in range(N):
+            board.append([0])
+    return board
+
+
+def add_queen(board, row, col):
+    """Sets "queen," or 1, to coordinates"""
+    board[row][col] = 1
+
+
+def new_queen_safe(board, row, col):
+    """For the board given, checks that for a new queen"""
+    x = row
+    y = col
+
+    for i in range(1, N):
+        if (y - i) >= 0:
+            # check up-left diagonal
+            if (x - i) >= 0:
+                if board[x - i][y - i]:
+                    return False
+            # check left
+            if board[x][y - i]:
+                return False
+            # check down-left diagonal
+            if (x + i) < N:
+                if board[x + i][y - i]:
+                    return False
     return True
 
-def solve_nqueens(n):
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
 
-    board = [[0 for _ in range(n)] for _ in range(n)]
-    solutions = []
+def coordinate_format(candidates):
+    """Converts a board (matrix of 1 and 0)"""
+    chess = []
+    for x, attempt in enumerate(candidates):
+        chess.append([])
+        for i, row in enumerate(attempt):
+            chess[x].append([])
+            for j, col in enumerate(row):
+                if col:
+                    chess[x][i].append(i)
+                    chess[x][i].append(j)
+    return chess
 
-    def solve(row):
-        if row == n:
-            solutions.append(["".join("Q" if cell == 1 else "." for cell in row) for row in board])
-            return
+candidates = []
+candidates.append(board_column_gen())
 
-        for col in range(n):
-            if is_safe(board, row, col, n):
-                board[row][col] = 1
-                solve(row + 1)
-                board[row][col] = 0
-
-    solve(0)
-
-    for solution in solutions:
-        for row in solution:
-            print(row)
-        print()
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-
-    solve_nqueens(N)
+for col in range(N):
+    new_candidates = []
+    for matrix in candidates:
+        for row in range(N):
+            if new_queen_safe(matrix, row, col):
+                temp = [line[:] for line in matrix]
+                add_queen(temp, row, col)
+                if col < N - 1:
+                    board_column_gen(temp)
+                new_candidates.append(temp)
+for item in coordinate_format(candidates):
+    print(item)
