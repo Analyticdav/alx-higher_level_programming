c9ac9c1a6996beb710dd7436ce5fe938e55b7ff9
David Akpan <Davidakpan247@gmail.com>
2023-10-03 10:50:41 -0700

    update

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,7 +5,6 @@
 class LockedClass:
     __slots__ = ["first_name"]
 
-    def __init__(self):
+def __init__(self):
         """This Creates new instances of Locked Class."""
-
-        self.first_name = "first_name"
+    self.first_name = "first_name"
