b97f26eb3bfcc487bf459b4f04a575bda216fa7b
David Akpan <Davidakpan247@gmail.com>
2023-10-03 05:10:16 -0700

    update

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,8 @@
-LockedClass:
-    def __setattr__(self, name, value):
-        if name == 'first_name':
-            self.__dict__[name] = value
-        else:
-            raise AttributeError("Cannot create instance")
+
+#!/usr/bin/python3
+"""Defines a locked class."""
+
+
+class LockedClass:
+
+    __slots__ = ["first_name"]
