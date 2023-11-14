68bcff0b1b2ca7b1e5e829d3f6db280a79dcc835
David Akpan <Davidakpan247@gmail.com>
2023-11-14 04:50:33 -0800

    Selecting scores in descending order

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- A script to list scores greater than 10 from second_table
-SELECT * FROM second_table WHERE score >= '10 ORDER BY score DESC'
+SELECT score, name FROM second_table WHERE score >= '10 ORDER BY score DESC'
