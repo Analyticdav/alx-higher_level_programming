cd0d29f1047313166139b45aa5843aa8ac175c2f
David Akpan <Davidakpan247@gmail.com>
2023-11-15 11:07:53 -0800

    granting all permission to users

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
 -- A script to grant all permissions to user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
