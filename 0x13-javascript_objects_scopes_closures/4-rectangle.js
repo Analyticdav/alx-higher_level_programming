8cb5dfaa059669e3bc3f42b1dd0d3022ba6e57dd
David Akpan <Davidakpan247@gmail.com>
2023-12-12 05:27:46 -0800

    rotation and double

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /*
-A class Rectangle that defines a rectangle:
+Write a class Rectangle that defines a rectangle:
 
 You must use the class notation for defining your class
 The constructor must take 2 arguments: w and h
@@ -8,6 +8,8 @@ Initialize the instance attribute width with the value of w
 Initialize the instance attribute height with the value of h
 If w or h is equal to 0 or not a positive integer, create an empty object
 Create an instance method called print() that prints the rectangle using the character X
+Create an instance method called rotate() that exchanges the width and the height of the rectangle
+Create an instance method called double() that multiples the width and the height of the rectangle by 2
 */
 class Rectangle {
   constructor (w, h) {
@@ -21,6 +23,17 @@ class Rectangle {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
   }
 }
 
