49e67d13bba4afe4a60514afd3503a2f4e7e1630
David Akpan <Davidakpan247@gmail.com>
2023-12-16 01:21:20 -0800

    update

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,10 +1,2 @@
 #!/usr/bin/node
-/*
-A  script that prints 3 lines:
-
-The first line: “C is fun”
-The second line: “Python is cool”
-The third line: “Javascript is amazing”
-*/
-
 console.log('C is fun\nPython is cool\nJavascript is amazing');
